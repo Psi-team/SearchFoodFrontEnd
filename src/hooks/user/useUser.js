52bc261158a431e11545d22ebb9d7fe29390daa9
dingq84 <dingq84@gmail.com>
2019-10-02 23:43:55 +0800

    modified browser short name

--- src/hooks/user/useUser.js
+++ src/hooks/user/useUser.js
@@ -6,19 +6,19 @@ import { apiLogin, apiSignup, apiLogout } from '../../apis';
 const getUserBrowser = () => {
   const sUsrAg = window.navigator.userAgent;
   if (sUsrAg.indexOf('Firefox') > -1) {
-    return 'Mozilla Firefox';
+    return 'Firefox';
   } else if (sUsrAg.indexOf("SamsungBrowser") > -1) {
     return 'Samsung Internet';
   } else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
     return 'Opera';
   } else if (sUsrAg.indexOf("Trident") > -1) {
-    return 'Microsoft Internet Explorer'
+    return 'Internet Explorer'
   } else if (sUsrAg.indexOf("Edge") > -1) {
-    return 'Microsoft Edge'
+    return 'Edge'
   } else if (sUsrAg.indexOf("Chrome") > -1) {
-    return 'Google Chrome or Chromium';
+    return 'Chrome';
   } else if (sUsrAg.indexOf("Safari") > -1) {
-    return 'Apple Safari';
+    return 'Safari';
   } else {
     console.error('unknown browser');
     return 'unknown';
